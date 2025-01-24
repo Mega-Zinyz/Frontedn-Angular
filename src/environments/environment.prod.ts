9d8e5fc4bfe8e3242b959e2ab455ea39c9d56d8d
Mega-Zinyz <megazinyz@gmail.com>
2025-01-24 21:06:14 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: process.env["BACKEND_NODEJS_URL"] || 'http://localhost:3000/api/auth', // Default fallback
+    apiUrl: process.env["NODE_API"], // Default fallback
   };
   
